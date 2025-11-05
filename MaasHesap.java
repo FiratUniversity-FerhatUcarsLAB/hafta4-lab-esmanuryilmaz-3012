7b68f08a658c6c67348177b6be8c27eb7949ef68
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-11-05 23:07:13 +0300

    Update MaasHesap.java

--- MaasHesap.java
+++ MaasHesap.java
@@ -2,7 +2,9 @@
      * Ad Soyad: [Esmanur YILMAZ]
      * Ogrenci No: [250541033]
      * Tarih: [05.11.2025]
-     * Aciklama: Gorev 2 - Maas Hesaplama Sistemi
-     * Bu program kullanicidan ogrenci bilgilerini alir ve
+     * Aciklama: Gorev 3 - Maas Hesaplama Sistemi
+     * Bu program bir calisandan; adi-soyadi, aylik brut maasi,
+     * haftalik calisma saati ve mesai saati gibi gerekli bilgileri alir 
+     * ve toplam gelir, kesintiler, net maasi hesaplayip sonuclari
      * duzenli bir formatta ekrana yazdirir.
      */
