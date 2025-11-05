5f3b8745b4b31aa11b571980eb8e8e03ef1a78b8
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-11-05 22:18:43 +0300

    Add header comments to GeometrikHesap.java
    Added header comments with author details and program description.

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,8 @@
-
+     /*
+     * Ad Soyad: [Esmanur YILMAZ]
+     * Ogrenci No: [250541033]
+     * Tarih: [05.11.2025]
+     * Aciklama: Gorev 3 - Geometrik Hesap Sistemi
+     * Bu program kullanicidan yaricap bilgisini alir ve gerekli islemleri yaptiktan
+     * sonra sonuclari duzenli bir formatta ekrana yazdirir.
+     */
