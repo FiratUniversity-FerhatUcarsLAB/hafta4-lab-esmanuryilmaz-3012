e44a8e51d743e8b074ec82d5ba35bba09818fcf6
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-11-05 22:16:58 +0300

    Update MaasHesap.java

--- MaasHesap.java
+++ MaasHesap.java
@@ -1,1 +1,8 @@
-
+/*
+     * Ad Soyad: [Esmanur YILMAZ]
+     * Ogrenci No: [250541033]
+     * Tarih: [05.11.2025]
+     * Aciklama: Gorev 2 - Maas Hesaplama Sistemi
+     * Bu program kullanicidan ogrenci bilgilerini alir ve
+     * duzenli bir formatta ekrana yazdirir.
+     */
