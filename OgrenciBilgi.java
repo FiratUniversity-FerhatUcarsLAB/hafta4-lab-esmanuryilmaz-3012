b3f602241b97cf0da17273fe802a5783c0250b32
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-11-05 22:22:15 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,7 +1,7 @@
      /*
      * Ad Soyad: [Esmanur YILMAZ]
      * Ogrenci No: [250541033]
-     * Tarih: [02.11.2025]
+     * Tarih: [05.11.2025]
      * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
      * Bu program kullanicidan ogrenci bilgilerini alir ve
      * duzenli bir formatta ekrana yazdirir.
